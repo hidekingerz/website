e7e5763432310fd6eec5113c3fb81e5d8225c2e6
hidekingerz <exthirix@gmail.com>
2023-11-09 00:48:12 +0900

    fix image

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -2,39 +2,6 @@ import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
 
-type FeatureItem = {
-  title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
-  description: JSX.Element;
-};
-
-const FeatureList: FeatureItem[] = [
-  {
-    title: '楽器',
-    Svg: require('@site/static/img/musical_instrument.svg').default,
-    description: (
-      <>
-        <p>バイオリンは主にクラシックをメインで５年目。<br></br>先生に師事して２年目。</p>
-        <p>ギターは1年目。</p>
-      </>
-    )
-  },
-];
-
-function Feature({title, Svg, description}: FeatureItem) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
-}
-
 export default function HomepageFeatures(): JSX.Element {
   return (
     <section className={styles.features}>
@@ -51,9 +18,18 @@ export default function HomepageFeatures(): JSX.Element {
               </p>
             </div>
           </div>
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
+          <div className={clsx('col col--4')}>
+            <div className="text--center">
+              <img className={styles.featureSvg} src={require('@site/static/img/musical_instrument.png').default} />
+            </div>
+            <div className="text--center padding-horiz--md">
+              <Heading as="h3">{'楽器'}</Heading>
+              <p>
+                <p>バイオリンは主にクラシックをメインで５年目。<br></br>先生に師事して２年目。</p>
+                <p>ギターは1年目。</p>
+              </p>
+            </div>
+          </div>
           <div className={clsx('col col--4')}>
             <div className="text--center">
               <img className={styles.featureSvg} src={require('@site/static/img/plastic_model.jpeg').default} />
